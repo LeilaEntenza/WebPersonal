72f2d1e8cdc248c62e28ea5fa64541190c3cc6e3
Lei <leilachiderski@gmail.com>
2024-08-04 21:50:48 -0300

    Diseño y backend empezados

--- obj/Debug/net8.0/WebPersonal.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebPersonal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebPersonal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd178f0db908d4c9c70d0d26fe966dd7308c7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8281c4b7ac268b17a09d2e62da1925d2b3a15079")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebPersonal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebPersonal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
